95844d8da598fe88f39f6ad6b45203f84df90ef0
karanianandkumar <karanianandkumar@yahoo.com>
2015-11-28 13:09:40 +0530

    5.Adding Actions to ToolBar_ActionBar_AppBar  Android Tutorials [HD 1080p]_2

--- Desktop/Udacity/MaterialDemo/app/src/main/java/com/example/android/materialdemo/SubActivity.java
+++ Desktop/Udacity/MaterialDemo/app/src/main/java/com/example/android/materialdemo/SubActivity.java
@@ -1,32 +1,32 @@
 package com.example.android.materialdemo;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
-public class MainActivity extends ActionBarActivity {
-
-    private Toolbar toolbar;
+public class SubActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sub);
 
 
-        toolbar = (Toolbar) findViewById(R.id.app_bar);
+        Toolbar toolbar=(Toolbar)findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
 
-    }
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+            }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -44,11 +44,11 @@ public class MainActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            Toast.makeText(this, "Just hit the" + item.getTitle(), Toast.LENGTH_SHORT).show();
+
             return true;
         }
-        if(id==R.id.navigate){
-            startActivity(new Intent(this,SubActivity.class));
+        if(id==android.R.id.home){
+            NavUtils.navigateUpFromSameTask(this);
         }
 
         return super.onOptionsItemSelected(item);
